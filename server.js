f93cebbc5a1c6369f9aceed6ee50488ee141f26b
lsmith32 <lmksmith97@gmail.com>
2022-04-17 18:06:48 -0500

    final commit

--- server.js
+++ server.js
@@ -10,6 +10,15 @@ app.use(express.static('public'));
 
 app.use(require('./routes'));
 
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network', {
+  //useFindAndModify: false,
+  useNewUrlParser: true,
+  //useUnifiedTopology: true
+});
+
+// Use this to log mongo queries being executed!
+mongoose.set('debug', true);
+
 // turn on connection to db and server
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
